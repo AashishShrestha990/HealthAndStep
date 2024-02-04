716e2f1d98e073349321dc1c0b0d57f193c16b3f
AashishShrestha990 <55848755+AashishShrestha990@users.noreply.github.com>
2024-02-04 16:52:56 +0545

    Login function

---
+++ app/src/main/java/coventry/aashish/healthnstep/LoginActivity.java
@@ -0,0 +1,38 @@
+package coventry.aashish.healthnstep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Login = (Button) findViewById(R.id.login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Toast.makeText(LoginActivity.this, "Sucessful", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
+    }
+}
