fd7871da027f16b8354e7ebc657c714054957f3a
AashishShrestha990 <55848755+AashishShrestha990@users.noreply.github.com>
2024-02-21 09:15:21 +0545

    Notification feature

--- app/src/main/java/coventry/aashish/healthnstep/MainActivity.java
+++ app/src/main/java/coventry/aashish/healthnstep/MainActivity.java
@@ -1,5 +1,7 @@
 package coventry.aashish.healthnstep;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -206,7 +209,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             waterprogress.setProgress(waterlevel);
 //            waterprogress,set(currentSteps);
             if(stepCount>=stepCountTarget){
-//                stepCounttargetView.setText("Achived");
+                NotificationCompat.Builder mBuilder = (NotificationCompat.Builder)
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.stepgreen)
+                        .setContentTitle("notification")
+                        .setContentText("You have achieved your goal");
+
+                NotificationManager notificationManager = (NotificationManager)
+                        getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(0,mBuilder.build());
             }
             float distanceinkm = currentSteps * stepLengthinmeter/1000;
             Distance.setText(String.format(Locale.getDefault(),"%.1f Km", distanceinkm));
